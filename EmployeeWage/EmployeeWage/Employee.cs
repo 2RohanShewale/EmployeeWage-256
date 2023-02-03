c945ae7475f89307e6ce64b96be52a97ca752a8a
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-03 09:48:10 +0530

    [Rohan]UC2:Daily Wage Calculator

--- EmployeeWage/EmployeeWage/Employee.cs
+++ EmployeeWage/EmployeeWage/Employee.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace EmployeeWage
 {
     public class Employee
     {
         const int present = 1;
+        const int WagePerHrs = 20;
         public void EmployeeAttendence()
         {
-            
+
+            int FullDayHrs;
             Random random = new Random();
             int empCheck = random.Next(0,2);
 
             if (empCheck == present)
+            {
                 Console.WriteLine("Employee is Present");
+                FullDayHrs = 8;
+            }
             else
+            {
+                FullDayHrs = 0;
                 Console.WriteLine("Employee is not Absent");
+            }
+
+            Console.WriteLine("Daily Wage : " + FullDayHrs * WagePerHrs);
         }
     }
 }
